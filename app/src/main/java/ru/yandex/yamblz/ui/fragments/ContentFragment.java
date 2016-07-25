fef20d5d0dfb674d25955896971fa4eb0c97ec2c
Veda Voronina <veda345@yandex.ru>
2016-07-25 20:44:41 +0300

    drawing

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -1,18 +1,77 @@
 package ru.yandex.yamblz.ui.fragments;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
+import org.xdty.preference.colorpicker.ColorPickerDialog;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ru.yandex.yamblz.R;
+import ru.yandex.yamblz.ui.views.MyView;
 
 public class ContentFragment extends BaseFragment {
+
+    @BindView(R.id.fam)
+    FloatingActionsMenu fam;
+    @BindView(R.id.my_view)
+    MyView myView;
+
+    private int curColor;
+
     @NonNull
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_content, container, false);
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
+        ButterKnife.bind(this, view);
+
+        FloatingActionButton fab1 = new FloatingActionButton(getActivity());
+        fab1.setIcon(R.drawable.ic_brush);
+        fam.addButton(fab1);
+
+        FloatingActionButton fab2 = new FloatingActionButton(getActivity());
+        fab2.setIcon(R.drawable.ic_pallette);
+        fab2.setOnClickListener(v -> {
+            int[] mColors = {Color.parseColor("#c52424"), Color.parseColor("#f4d26c"),
+                    Color.parseColor("#aeea00"), Color.parseColor("#1de9b6"),
+                    Color.parseColor("#1e88e5"), Color.parseColor("#5e35b1"), Color.parseColor("#e040fb"),
+                    Color.parseColor("#e91e63"), Color.parseColor("#f57f17"),
+                    Color.CYAN, Color.RED, Color.GREEN, Color.YELLOW};
+
+            ColorPickerDialog dialog = ColorPickerDialog.newInstance(R.string.color_picker_default_title,
+                    mColors,
+                    Color.YELLOW,
+                    5, // Number of columns
+                    ColorPickerDialog.SIZE_SMALL);
+
+            dialog.setOnColorSelectedListener((color) -> {
+               curColor = color;
+                myView.setColor(curColor);
+            });
+            dialog.show(((Activity) v.getContext()).getFragmentManager(), "color_dialog_test");
+        });
+        fam.addButton(fab2);
+
+        FloatingActionButton fab3 = new FloatingActionButton(getActivity());
+        fab3.setIcon(R.drawable.ic_save);
+        fam.addButton(fab3);
+
+        FloatingActionButton fab4 = new FloatingActionButton(getActivity());
+        fab4.setIcon(R.drawable.ic_load);
+        fam.addButton(fab4);
+        return view;
     }
 }
---
+++ app/src/main/java/ru/yandex/yamblz/ui/views/MyView.java
@@ -0,0 +1,90 @@
+package ru.yandex.yamblz.ui.views;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by vorona on 25.07.16.
+ */
+
+public class MyView extends View {
+    private Path drawPath;
+    private Paint drawPaint, canvasPaint;
+    private int paintColor = Color.BLACK;
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
+
+    public MyView(Context context) {
+        super(context);
+        setup();
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setup();
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setup();
+    }
+
+    private void setup() {
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return  true;
+    }
+
+    public void setColor(int newColor){
+        invalidate();
+        drawPaint.setColor(newColor);
+    }
+}
