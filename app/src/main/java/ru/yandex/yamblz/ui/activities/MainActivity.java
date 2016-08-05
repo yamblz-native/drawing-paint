af1ad59d1413b2cc1f822356a79d36668be23470
Timofey Talenfeld <taymon@yandex.ru>
2016-08-06 02:39:16 +0300

    Make HW

--- app/src/main/java/ru/yandex/yamblz/ui/activities/MainActivity.java
+++ app/src/main/java/ru/yandex/yamblz/ui/activities/MainActivity.java
@@ -3,6 +3,9 @@ package ru.yandex.yamblz.ui.activities;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -11,12 +14,15 @@ import ru.yandex.yamblz.App;
 import ru.yandex.yamblz.R;
 import ru.yandex.yamblz.developer_settings.DeveloperSettingsModule;
 import ru.yandex.yamblz.ui.fragments.ContentFragment;
+import ru.yandex.yamblz.ui.fragments.MenuFragment;
 import ru.yandex.yamblz.ui.other.ViewModifier;
 
 public class MainActivity extends BaseActivity {
 
     @Inject @Named(DeveloperSettingsModule.MAIN_ACTIVITY_VIEW_MODIFIER)
     ViewModifier viewModifier;
+
+    private DialogFragment menuFragment;
 
     @SuppressLint("InflateParams") // It's okay in our case.
     @Override
@@ -27,10 +33,32 @@ public class MainActivity extends BaseActivity {
         setContentView(viewModifier.modify(getLayoutInflater().inflate(R.layout.activity_main, null)));
 
         if (savedInstanceState == null) {
+            ContentFragment contentFragment = new ContentFragment();
+
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.main_frame_layout, new ContentFragment())
+                    .replace(R.id.main_frame_layout, contentFragment)
                     .commit();
+
+            menuFragment = MenuFragment.newInstance(contentFragment);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.draw:
+                menuFragment.show(getSupportFragmentManager(), menuFragment.getClass().getCanonicalName());
+                return true;
+            default:
+                return false;
+        }
+    }
+
 }
--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -7,12 +7,47 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
 import ru.yandex.yamblz.R;
+import ru.yandex.yamblz.ui.views.CanvasView;
 
-public class ContentFragment extends BaseFragment {
+public class ContentFragment extends BaseFragment implements MenuFragment.CanvasFragment {
+
+    @BindView(R.id.canvas_view) CanvasView canvasView;
+
     @NonNull
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_content, container, false);
+    }
+
+    @Override
+    public void setColor(int color) {
+        canvasView.setColor(color);
+    }
+
+    @Override
+    public void setTool(CanvasView.Tool tool) {
+        canvasView.setTool(tool);
+    }
+
+    @Override
+    public void setPrint(int drawableId) {
+        canvasView.setPrint(drawableId);
+    }
+
+    @Override
+    public void save() {
+        canvasView.saveBitmap();
+    }
+
+    @Override
+    public void load() {
+        canvasView.loadBitmap();
+    }
+
+    @Override
+    public void applyFilter(CanvasView.Filter filter) {
+        canvasView.applyFilter(filter);
     }
 }
